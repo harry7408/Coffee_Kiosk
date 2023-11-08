56885f5ccc1fb77b15e9ad56525b4159443a5542
harry <harry7408@naver.com>
2023-11-08 16:59:33 +0900

    [Feat] #1 Splash 이미지 및 Home화면 작업

---
+++ app/src/main/java/com/choi/coffee_kiosks/view/KioskMainFragment.kt
@@ -0,0 +1,10 @@
+package com.choi.coffee_kiosks.view
+
+import com.choi.coffee_kiosks.base.BaseFragment
+import com.choi.coffee_kiosks.databinding.FragmentKioskMainBinding
+
+class KioskMainFragment :
+    BaseFragment<FragmentKioskMainBinding>(FragmentKioskMainBinding::inflate) {
+
+
+}
