11205c2c1b6f9c8df9ef05d353cd37637164be4b
harry <harry7408@naver.com>
2023-12-08 22:04:10 +0900

    feat: 성공 및 실패 Lottie 추가

--- app/src/main/java/com/choi/coffee_kiosks/view/practice/result/FailureFragment.kt
+++ app/src/main/java/com/choi/coffee_kiosks/view/practice/result/FailureFragment.kt
@@ -1,8 +1,25 @@
 package com.choi.coffee_kiosks.view.practice.result
 
+import android.os.Bundle
+import android.view.View
+import com.choi.coffee_kiosks.R
 import com.choi.coffee_kiosks.base.BaseFragment
 import com.choi.coffee_kiosks.databinding.FragmentFailureBinding
+import com.choi.coffee_kiosks.view.home.HomeFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class FailureFragment: BaseFragment<FragmentFailureBinding>(FragmentFailureBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000L)
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer,HomeFragment())
+                .commit()
+        }
+    }
 
 }
--- app/src/main/java/com/choi/coffee_kiosks/view/practice/result/SuccessFragment.kt
+++ app/src/main/java/com/choi/coffee_kiosks/view/practice/result/SuccessFragment.kt
@@ -1,7 +1,24 @@
 package com.choi.coffee_kiosks.view.practice.result
 
+import android.os.Bundle
+import android.view.View
+import com.choi.coffee_kiosks.R
 import com.choi.coffee_kiosks.base.BaseFragment
 import com.choi.coffee_kiosks.databinding.FragmentSuccessBinding
+import com.choi.coffee_kiosks.view.home.HomeFragment
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SuccessFragment : BaseFragment<FragmentSuccessBinding>(FragmentSuccessBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000L)
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, HomeFragment())
+                .commit()
+        }
+    }
 }
